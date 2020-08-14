2a6a07f9f272df3485b256e92cce6e992f4f513e
HamidBehnam <hamid.behnam@gmail.com>
2020-08-14 16:13:09 +0000

    adding the build files to the dest repo

--- polyfills-es5.js
+++ polyfills-es5.js
@@ -18899,7 +18899,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! /var/lib/jenkins/workspace/ev-mobile-dev-env-angular_master/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
+    /*! /var/lib/jenkins/workspace/amidev-mobile-dev-env-angular_qa/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -18907,7 +18907,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     module.exports = __webpack_require__(
-    /*! /var/lib/jenkins/workspace/ev-mobile-dev-env-angular_master/src/polyfills.ts */
+    /*! /var/lib/jenkins/workspace/amidev-mobile-dev-env-angular_qa/src/polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
